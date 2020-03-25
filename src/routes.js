b5fecc788442fde5c0a8befcf08ff56129f4f3b2
heitordeve <heitor.developer@outlook.com>
2020-03-25 00:50:39 -0300

    Development - Added Controllers, Routes and cors

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,27 @@
 const express = require('express');
 
-const routes = express.routes();
+//Import Controller
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+//CreateSession
+routes.post('/sessions', SessionController.create);
+
+//Group routes ONgs
+routes.get('/ongs', OngController.index); //List all ongs
+routes.post('/ongs', OngController.store); //Create ongs
+
+//Group routes Incidents
+routes.get('/incidents', IncidentsController.index); //List all Incidents
+routes.post('/incidents', IncidentsController.store); //Create Incidents
+routes.delete('/incidents/:id', IncidentsController.delete); //Delete Incidents
 
 
-routes.post('/users', (req, res) => {
-    return res.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'Heitor Carlos'
-    });
-});
+//Group routes ProfileController
+routes.get('/profile', ProfileController.index); //List specific incidents ongs
 
 module.exports = routes;
